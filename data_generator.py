2bc1e072633c23c86ad113380449fd6cda73b86d
Rodrigo Bermudez Schettino <rodrigobermudezschettino@gmail.com>
2021-02-20 15:37:36 +0100

    Data gen: Remove trailing whitespaces
    Used function in VSCode to automatically remove them.

--- data_generator.py
+++ data_generator.py
@@ -1,6 +1,6 @@
 import torch
 # %%
-# Adapted from: 
+# Adapted from:
 # https://colab.research.google.com/github/deepmind/neural-processes/blob/master/attentive_neural_process.ipynb#scrollTo=Px-atGEfNnWT
 # Copyright 2019 Google LLC
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
@@ -12,20 +12,20 @@ class GPCurves:
     Supports vector inputs (x) and vector outputs (y). A (scaled) RBF Kernel is used to
     output independent Gaussian Processes.
     (*) Scale parameters sigma and length_scale can be passed to define a specific RBF Kernel
-    otherwise the parameters will be sampled uniformly within [0., length_scale) and [0., sigma_scale)   
+    otherwise the parameters will be sampled uniformly within [0., length_scale) and [0., sigma_scale)
     """
-    
-    def __init__(self, 
+
+    def __init__(self,
                 batch_size,
                 max_num_context,
-                x_size=1, 
+                x_size=1,
                 y_size=1,
                 sigma_scale=1.0,
                 length_scale=1.0,
                 random_params=True,
                 testing=False
                 ):
-    
+
         """Creates a regression dataset of functions sampled from a GP.
 
         Args:
@@ -35,10 +35,10 @@ class GPCurves:
             y_size: Integer >= 1 for length of "y values" vector.
             length_scale: Float; typical scale for kernel distance function.
             sigma_scale: Float; typical scale for variance.
-            random_params: If `True`, the kernel parameters (length and sigma) 
+            random_params: If `True`, the kernel parameters (length and sigma)
             will be sampled uniformly within [0.1, length_scale] and [0.1, sigma_scale].
         """
-        
+
         self.batch_size = batch_size
         self.max_num_context = max_num_context
         self.x_size = x_size
@@ -63,7 +63,7 @@ class GPCurves:
             Returns:
             The kernel, a float tensor of shape
             [B, y_size, num_total_points, num_total_points].
-    
+
         """
         num_total_points = X.size()[1]
 
@@ -75,15 +75,15 @@ class GPCurves:
         # [B, y_size, num_total_points, num_total_points, x_size]
         # None indexing [None, :] acts like tensor.unsqueeze(dim)
         norm = torch.square(diff[:, None, :, :, :] / l1[:, :, None, None, :])
-        
+
         # [B, data_size, num_total_points, num_total_points]
-        norm = norm.sum(-1) # one data point per row 
+        norm = norm.sum(-1) # one data point per row
 
         # [B, y_size, num_total_points, num_total_points]
         kernel = torch.square(sigma)[:, :, None, None] * torch.exp(-0.5 * norm)
-        
+
         # Add some noise to the diagonal to make the cholesky work.
-        
+
         kernel.add_(torch.eye(num_total_points).mul(noise**2))
         #TODO might result in wrong dimensions
 
@@ -94,7 +94,7 @@ class GPCurves:
         """Builds the op delivering the data.
 
         Generated functions are `float32` with x values between -2 and 2.
-        
+
         Returns:
 
         """
@@ -104,30 +104,30 @@ class GPCurves:
         if self.testing:
             num_target = 401
             num_total_points = num_target
-            X = torch.range(-2, 2, 1. / 100).unsqueeze(0).expand(self.batch_size, -1) 
-            # attention! returns view - copy necessary if in place operations are used 
+            X = torch.range(-2, 2, 1. / 100).unsqueeze(0).expand(self.batch_size, -1)
+            # attention! returns view - copy necessary if in place operations are used
             X.unsqueeze_(-1)
-            
+
         else:
             num_target = torch.randint(0, self.max_num_context - num_context, [])
             num_total_points = num_context + num_target
             X = torch.Tensor(self.batch_size, num_total_points, self.x_size).uniform_(-2, 2)
-            
+
 
         #set Kernel parameters randomly for every batch
         if self.random_params:
             length = torch.Tensor(self.batch_size, self.y_size, self.x_size).uniform_(0.1, self.length_scale)
             sigma = torch.Tensor(self.batch_size, self.y_size).uniform_(0.1, self.sigma_scale)
-        
+
         else:
         #use the same Kernel parameters for every batch
             length = torch.ones(self.batch_size, self.y_size, self.x_size).mul_(self.length_scale)
             sigma = torch.ones(self.batch_size, self.y_size).mul_(self.sigma_scale)
-        
+
 
         kernel = self._kernel(X, length, sigma)
         cholesky = kernel.double().cholesky().float() # TODO (maybe): change precision to float64 and cast to float32 afterwards
-        y = cholesky.matmul(torch.randn(self.batch_size, self.y_size, num_total_points, 1)) 
+        y = cholesky.matmul(torch.randn(self.batch_size, self.y_size, num_total_points, 1))
         #sampling with no mean assumption: y = mu + sigma*z~N(0,I) ~ c.L * rand_normal([0, 1]) with appropriate shape
         #TODO if runtime error: change dimension -1 of torch.randn_like(cholesky) to ?
 
@@ -142,7 +142,7 @@ class GPCurves:
             idx = torch.randperm(num_target)
             context_x = torch.index_select(X, 1, idx[:num_context]) # tf.gather(x_values, idx[:num_context], axis=1)
             context_y = torch.index_select(Y, 1, idx[:num_context]) # tf.gather(y_values, idx[:num_context], axis=1)
-        
+
         else:
             # Select the targets which will consist of the context points as well as
             # some new target points
