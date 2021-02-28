462770abd014a925da52db7efddae28940b5b3d9
Rodrigo Bermudez Schettino <rodrigobermudezschettino@gmail.com>
2021-02-28 17:50:55 +0100

    Utils: Add missing plot functions
    These plot functions were only present in python notebook.

--- neural_processes/utils.py
+++ neural_processes/utils.py
@@ -94,3 +94,101 @@ def plot_1d(context_x, context_y, target_x, target_y, pred_y, std):
 
 def plot_2d(context_x, context_y, target_x, target_y, pred_y, std):
     return None
+
+
+def plot_functions(target_x, target_y, context_x, context_y, pred_y, std):
+  """Plots the predicted mean and variance and the context points.
+
+  Args:
+    target_x: An array of shape [B,num_targets,1] that contains the
+        x values of the target points.
+    target_y: An array of shape [B,num_targets,1] that contains the
+        y values of the target points.
+    context_x: An array of shape [B,num_contexts,1] that contains
+        the x values of the context points.
+    context_y: An array of shape [B,num_contexts,1] that contains
+        the y values of the context points.
+    pred_y: An array of shape [B,num_targets,1] that contains the
+        predicted means of the y values at the target points in target_x.
+    std: An array of shape [B,num_targets,1] that contains the
+        predicted std dev of the y values at the target points in target_x.
+  """
+  # Plot everything
+  plt.plot(target_x, pred_y[0].unsqueeze(-1), 'b', linewidth=2)
+  plt.plot(target_x, target_y, 'k:', linewidth=2)
+  plt.plot(context_x, context_y, 'ko', markersize=10)
+  plt.fill_between(
+      target_x,
+      pred_y[0].unsqueeze(-1) - std[0],
+      pred_y[0].unsqueeze(-1) + std[0],
+      alpha=0.2,
+      facecolor='#65c9f7',
+      interpolate=True)
+
+  # Make the plot pretty
+  plt.yticks([-2, 0, 2], fontsize=16)
+  plt.xticks([-2, 0, 2], fontsize=16)
+  plt.ylim([-2, 2])
+  plt.grid('off')
+  ax = plt.gca()
+  plt.show()
+
+def plot_functions2(target_x, target_y, context_x, context_y, pred_y, std):
+  """Plots the predicted mean and variance and the context points.
+
+  Args:
+    target_x: An array of shape [B,num_targets,1] that contains the
+        x values of the target points.
+    target_y: An array of shape [B,num_targets,1] that contains the
+        y values of the target points.
+    context_x: An array of shape [B,num_contexts,1] that contains
+        the x values of the context points.
+    context_y: An array of shape [B,num_contexts,1] that contains
+        the y values of the context points.
+    pred_y: An array of shape [B,num_targets,1] that contains the
+        predicted means of the y values at the target points in target_x.
+    std: An array of shape [B,num_targets,1] that contains the
+        predicted std dev of the y values at the target points in target_x.
+  """
+  # Plot everything
+  plt.plot(target_x[0], pred_y[0], 'b', linewidth=2)
+  plt.plot(target_x[0], target_y[0], 'k:', linewidth=2)
+  plt.plot(context_x[0], context_y[0], 'ko', markersize=10)
+  plt.fill_between(
+      target_x[0, :, 0],
+      pred_y[0, :, 0] - std[0, :, 0],
+      pred_y[0, :, 0] + std[0, :, 0],
+      alpha=0.2,
+      facecolor='#65c9f7',
+      interpolate=True)
+
+  # Make the plot pretty
+  plt.yticks([-2, 0, 2], fontsize=16)
+  plt.xticks([-2, 0, 2], fontsize=16)
+  plt.ylim([-2, 2])
+  plt.grid('off')
+  ax = plt.gca()
+  plt.show()
+
+
+def unwrap(data, list_, batch_size):
+    """unwrappes data which is present in format [batch_size, num_points, data_size]"""
+
+    for i, d in enumerate(data):
+          for j, x in enumerate(d):
+              list_[j].append(x.to(dev)) # torch.reshape(x, (z * y, 1)))
+
+def preprocess(gp, niter, batch_size):
+
+    list_ = [[] for _ in range(niter * batch_size)]
+
+    start = 0
+    stop = 1
+    for i in range(niter):
+        # data.shape = [batch_size, no_of_elements_representing_one_function/curve, dim_of_x=1]
+        data = gp.generate_curves()
+        unwrap(data, list_[start*batch_size : stop*batch_size], batch_size)
+
+        start += 1
+        stop += 1
+    return list_
