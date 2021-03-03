29c2494ed61d458e22549a74e465d3ff6f0fb86c
Rodrigo Bermudez Schettino <rodrigobermudezschettino@gmail.com>
2021-03-03 16:50:45 +0100

    Python: Delete duplicated functions from utils.py
    Functions were refactored to plot.py and mnist.py.

--- neural_processes/utils.py
+++ neural_processes/utils.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import matplotlib.pyplot as _plt
 import torch as _torch
 from torch import distributions as _distributions
 
@@ -57,120 +56,42 @@ def fit(niter, save_iter, np, opt, train_set, query_test):
     return mu, sigma
 
 
-# taken from colab NP
-def plot_1d(context_x, context_y, target_x, target_y, pred_y, std):
-    """Plots the predicted mean and variance and the context points.
+# rescale coordinates to range [0, 28*28)
+# rescale y to [0, 1]
 
-    Args:
-      target_x: An array of shape [B,num_targets,1] that contains the
-          x values of the target points.
-      target_y: An array of shape [B,num_targets,1] that contains the
-          y values of the target points.
-      context_x: An array of shape [B,num_contexts,1] that contains
-          the x values of the context points.
-      context_y: An array of shape [B,num_contexts,1] that contains
-          the y values of the context points.
-      pred_y: An array of shape [B,num_targets,1] that contains the
-          predicted means of the y values at the target points in target_x.
-      std: An array of shape [B,num_targets,1] that contains the
-          predicted std dev of the y values at the target points in target_x.
-    """
-    # Plot everything
-    _plt.plot(target_x[0], pred_y[0], 'b', linewidth=2)
-    _plt.plot(target_x[0], target_y[0], 'k:', linewidth=2)
-    _plt.plot(context_x[0], context_y[0], 'ko', markersize=10)
-    _plt.fill_between(
-        target_x[0, :, 0],
-        pred_y[0, :, 0] - std[0, :, 0],
-        pred_y[0, :, 0] + std[0, :, 0],
-        alpha=0.2,
-        facecolor='#65c9f7',
-        interpolate=True)
+def rescale(x, y, dev):
+    scale_x = 28 * 28 - 1
 
-    # Make the plot pretty
-    _plt.yticks([-2, 0, 2], fontsize=16)
-    _plt.xticks([-2, 0, 2], fontsize=16)
-    _plt.ylim([-2, 2])
-    _plt.grid('off')
-    ax = _plt.gca()
-    _plt.show()
+    new_y = y + .5
+    new_y = new_y.to(dev)
+
+    new_x = (x + 1).div(2) * scale_x
+    new_x = new_x.round()
+    new_x = new_x.long().to(dev)
+
+    return new_x, new_y
+
+# input is of shape as NP input excluding batch_dim, s.t. [num_points, [row_idx, col_idx]]
+# returns image of shape [1, 28, 28]
 
 
-def plot_2d(context_x, context_y, target_x, target_y, pred_y, std):
-    return None
+def map_to_img(xc, yc, xt, yt, dev):
 
+    img = torch.zeros(28, 28)
+    img = img.to(dev)
 
-def plot_functions(target_x, target_y, context_x, context_y, pred_y, std):
-    """Plots the predicted mean and variance and the context points.
+    num_context = xc.shape[0]
 
-    Args:
-      target_x: An array of shape [B,num_targets,1] that contains the
-          x values of the target points.
-      target_y: An array of shape [B,num_targets,1] that contains the
-          y values of the target points.
-      context_x: An array of shape [B,num_contexts,1] that contains
-          the x values of the context points.
-      context_y: An array of shape [B,num_contexts,1] that contains
-          the y values of the context points.
-      pred_y: An array of shape [B,num_targets,1] that contains the
-          predicted means of the y values at the target points in target_x.
-      std: An array of shape [B,num_targets,1] that contains the
-          predicted std dev of the y values at the target points in target_x.
-    """
-    # Plot everything
-    _plt.plot(target_x, pred_y[0].unsqueeze(-1), 'b', linewidth=2)
-    _plt.plot(target_x, target_y, 'k:', linewidth=2)
-    _plt.plot(context_x, context_y, 'ko', markersize=10)
-    _plt.fill_between(
-        target_x,
-        pred_y[0].unsqueeze(-1) - std[0],
-        pred_y[0].unsqueeze(-1) + std[0],
-        alpha=0.2,
-        facecolor='#65c9f7',
-        interpolate=True)
+    # TODO use numpy/pytorch indexing style
+    for i, idx in enumerate(xt):
 
-    # Make the plot pretty
-    _plt.yticks([-2, 0, 2], fontsize=16)
-    _plt.xticks([-2, 0, 2], fontsize=16)
-    _plt.ylim([-2, 2])
-    _plt.grid('off')
-    ax = _plt.gca()
-    _plt.show()
+        if i < num_context:
+            y = yc[i]
 
+        # for test reasons ignore predictions for known context_y and use true values
+        else:
+            y = yt[i]
+        # y = yt[i]
+        img[idx[0], idx[1]] = y
 
-def plot_functions2(target_x, target_y, context_x, context_y, pred_y, std):
-    """Plots the predicted mean and variance and the context points.
-
-    Args:
-      target_x: An array of shape [B,num_targets,1] that contains the
-          x values of the target points.
-      target_y: An array of shape [B,num_targets,1] that contains the
-          y values of the target points.
-      context_x: An array of shape [B,num_contexts,1] that contains
-          the x values of the context points.
-      context_y: An array of shape [B,num_contexts,1] that contains
-          the y values of the context points.
-      pred_y: An array of shape [B,num_targets,1] that contains the
-          predicted means of the y values at the target points in target_x.
-      std: An array of shape [B,num_targets,1] that contains the
-          predicted std dev of the y values at the target points in target_x.
-    """
-    # Plot everything
-    _plt.plot(target_x[0], pred_y[0], 'b', linewidth=2)
-    _plt.plot(target_x[0], target_y[0], 'k:', linewidth=2)
-    _plt.plot(context_x[0], context_y[0], 'ko', markersize=10)
-    _plt.fill_between(
-        target_x[0, :, 0],
-        pred_y[0, :, 0] - std[0, :, 0],
-        pred_y[0, :, 0] + std[0, :, 0],
-        alpha=0.2,
-        facecolor='#65c9f7',
-        interpolate=True)
-
-    # Make the plot pretty
-    _plt.yticks([-2, 0, 2], fontsize=16)
-    _plt.xticks([-2, 0, 2], fontsize=16)
-    _plt.ylim([-2, 2])
-    _plt.grid('off')
-    ax = _plt.gca()
-    _plt.show()
+    return img
